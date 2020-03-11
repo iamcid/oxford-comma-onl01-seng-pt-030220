cfc69e57a299b10637f567168e9fb4ce2c208271
Christopher Cid <iamcid17@gmail.com>
2020-03-11 03:34:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,11 @@ def oxford_comma(array)
   array.size == 1
    return array.join
   if array.size >= 2
-    return array[0].join + "and" + array [1]
+    return array[0] + "and" + array [1]
   else array.size == 3
+    array.each_with_index do |fruit, index|
+      return +="and"+ fruit if index == array.size - 1
+      break if index == array.size - 1
+      string +=
   end
 end
