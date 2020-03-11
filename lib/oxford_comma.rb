ea5613ff7817832758ead3e944020a22460bce64
Christopher Cid <iamcid17@gmail.com>
2020-03-11 03:28:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   array.size == 1
    return array.join
-  if array.size == 2
+  if array.size >= 2
     return array[0].join + "and" + array [1]
   else array.size == 3
   end
